9fd9ed140d554a5f46e9376b79cf6370012f41e7
Aics7 <43910216+Aics7@users.noreply.github.com>
2019-01-18 19:44:52 +0000

    Update StringLength.java

--- StringLength.java
+++ StringLength.java
@@ -2,17 +2,18 @@
 public class StringLength {  
 	/**this method accepts a string (input) as an input and returns the length of the string*/
 	public static int stringLen ( String input ) {
-		
+		//the next line of code is the statement required by the question 
 		int strLength = input.length(); //This line stores the length of the input
 		
 		if (strLength < 0) {
-			System.out.println("Inputed string is invalid"); // Checks whether string has element(s) or niot
+			System.out.println("Inputed string is invalid"); // Checks whether string has element(s) or not
 		}
 		if (strLength >= 0) {
 			System.out.println("String contains an element(s)!"); 
 		}
 			return strLength;
 	}
+	//main method to test the code
     public static void main(String[] args) {  
     	System.out.println(StringLength.stringLen("Michael and Issifu"));
     
