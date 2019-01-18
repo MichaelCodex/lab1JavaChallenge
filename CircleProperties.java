55aabc96ec90d6b1f85cc06e490c339e8e700136
Aics7 <43910216+Aics7@users.noreply.github.com>
2019-01-18 19:24:34 +0000

    Update CircleProperties.java

--- CircleProperties.java
+++ CircleProperties.java
@@ -1,17 +1,15 @@
 import java.lang.Math;
 import java.util.Scanner;
-
+/** this code  asks the user for the radius of a circle and calculates  its area and circumference*/
 public class CircleProperties {
-	
 	public static void main(String[] args) {
-		
-	Scanner reader = new Scanner(System.in);
-        System.out.println("Enter your radius:");
+	Scanner reader = new Scanner(System.in);//creating an instance of the scanner class to read user input
+        System.out.println("Enter your radius:");//asking the user for input
         double radius = reader.nextDouble();
-        reader.close();
+        reader.close();//closes scanner
 		
-        double circleArea= (Math.PI * (radius * radius));
-        double circleCircumference= (2* Math.PI * radius);
+        double circleArea= (Math.PI * (radius * radius));//calculation of area
+        double circleCircumference= (2* Math.PI * radius);//calculaton of circumference
         
         System.out.println(String.format("%.2f",circleArea));
         System.out.println(String.format("%.2f",circleCircumference));
