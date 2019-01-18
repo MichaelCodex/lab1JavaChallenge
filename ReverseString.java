34f8b9a09e9b00255ca564ea31f70744f4596619
MichaelCodex <46810796+MichaelCodex@users.noreply.github.com>
2019-01-18 19:24:30 +0000

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class ReverseString{
 	
 	    public static void main(String[] args) {
+		    //The scanner reader class allows us to read an input (string) from the user.
 	        Scanner read = new Scanner(System.in);
 	        System.out.println("Enter string to reverse:");
 	        String str = read.nextLine();
 	        String reverse = "";
 	        read.close();
 	        
-	        String arr[] = str.split(" ");
+	        String arr[] = str.split(" "); // This line splits the inputed string into an array (words).
 	        for (int i = 0; i < arr.length; i++) {
-	        	 for (int j = arr[i].length() - 1; j >= 0; j--) {
-	        		 reverse = reverse + arr[i].charAt(j);
+	        	 for (int j = arr[i].length() - 1; j >= 0; j--) {  // This line reverses each character in the respective
+	        		 reverse = reverse + arr[i].charAt(j);     // arrays.
 	            }
-	        	 reverse = reverse + " ";
+	        	 reverse = reverse + " "; // This allows for a space to be included between the words after the 
+					           // concatenation of the arrays.
 	        }
 	        System.out.println("Reversed string is: " + reverse);	
 	       }	 
